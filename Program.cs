c0e575690d3135550eeef2a0ff79fc646f3d3efe
Saadu02 <111046257+Saadu02@users.noreply.github.com>
2022-09-22 11:12:46 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Security;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Channels;
 
 namespace LA_1100
@@ -9,13 +10,12 @@ namespace LA_1100
         static void Main(string[] args)
         {
             // Eingabe
-
-            int zufaelligezahl = new Random().Next(1, 100);
-            int i = Convert.ToInt32(zufaelligezahl);
-            bool wiederspielen = false;
+            
+            Random zuefaelligeZahl = new Random(); 
+            int i = zuefaelligeZahl.Next(1, 100);
+            bool wiederspielen = true;
             bool win = false;
             int versuche = 0;
-
             //Verarbeitung
 
             try
@@ -24,67 +24,74 @@ namespace LA_1100
                 {
                     do
                     {
-
                         Console.Write("Wähle eine Zahl aus zwischen 1 bis 100: ");
                         int eingabezahl = Convert.ToInt32(Console.ReadLine());
-            
-            
 
                         if (eingabezahl < i)
                         {
+                            Console.ForegroundColor = ConsoleColor.Magenta;
                             Console.WriteLine("Die geratene Zahl ist niedriger als die Geheimzahl.");
+                            Console.ResetColor();
                             versuche++;
                         }
                         else if (eingabezahl > i)
                         {
+                            Console.ForegroundColor = ConsoleColor.DarkMagenta;
                             Console.WriteLine("Die geratene Zahl ist grösser als die Geheimzahl.");
+                            Console.ResetColor();
                             versuche++;
                         }
                         else if (eingabezahl < 0 | eingabezahl > 100)
                         {
-                            Console.Write("Fehler! Bitte erneut eingeben. ");                            
+                            Console.Write("Fehler! Bitte erneut eingeben. ");
                             versuche++;
                         }
                         else if (eingabezahl == i)
                         {
+                            versuche++;
+                            Console.ForegroundColor = ConsoleColor.Green;
                             Console.WriteLine("Bravo! Du hast im Spiel mit " + versuche + " Versuche geschafft die richtige zufällige Zahl herauszufinden.");
+                            Console.ResetColor();
                             win = true;
-                        }
+
+                            Console.Write("Möchten Sie noch Mals spielen? [y|n]: ");
+                            char Antwort = Convert.ToChar(Console.ReadLine());                            
+
+                            if (Antwort == 'y')
+                            {                     
+                              wiederspielen = true;
+                                i = zuefaelligeZahl.Next(1, 100);
+                                versuche = 0;  
+                            }
+                            else
+                            {
+                                wiederspielen = false;                                
+                            }
+
+                        }   
 
                         Console.WriteLine();
                     } while (win == false);
 
-                    //Ausgabe
-
-                    Console.Write("Möchten Sie noch Mals spielen? [y|n]: ");
-                    char Antwort = Convert.ToChar(Console.ReadLine());
-
-                    if (Antwort == 'y')
-                    {
-                        wiederspielen = true;
-                    }
-                    else
-                    {
-                        wiederspielen = false;
-
-                    }
-
+                    //Ausgabe      
                 } while (wiederspielen);
-
-
-                Console.WriteLine("Danke fürs Spielen. Aufwiedersehen :)");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Danke fürs Spielen. Aufwiedersehen :) ");
+                Console.ResetColor();
 
                 Console.ReadKey();
-
             }
             catch
             {
-                Console.WriteLine("Ungültige Eingabe. Wähle Sie eine Zahl aus zwischen 1 bis 100: ");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Ungültige Eingabe. Bitte starten Sie das Programm neu! ");
+                Console.ResetColor();
                 Environment.Exit(0);
-                //Console.WriteLine(wiederspielen);
-            } 
-                    
-            
+                
+            }
+
         }
+
     }
 }           
